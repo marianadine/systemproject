1b3f1904bd4b0682dec6a439d3ad498a80a1ef33
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2024-10-16 21:32:37 +0800

    commit

--- systemfinals/src/components/HomePage.js
+++ systemfinals/src/components/HomePage.js
@@ -5,6 +5,7 @@ import './components_css/homepagestyle.css';
 import NavBar from './NavBar';
 import ScrollToTopButton from './ScrollToTopButton';
 
+import logocontact from '../imgs/websitelogo.png';
 import f1 from "../imgs/f1.png";
 import f2 from "../imgs/f2.png";
 import f3 from "../imgs/f3.png";
@@ -103,8 +104,35 @@ const HomePage = () => {
       </section>
 
       <section id='contactsection'>
-        <h1>hello</h1>
-      </section>
+        <img src={logocontact} alt="Logo" />
+        <h1>Support our socials!</h1>
+        <p>Stay connected and up-to-date with the latest news, offers, and updates. Follow us on our social media channels and help us grow!</p>
+        
+        <div className='contactbox'>            
+            <div className='content1'>
+                <h1>NU MOA Bulldogs Exchange</h1>
+                <p>Your one-stop shop for exclusive NU MOA merchandise and more! Pre-order, pick up, and represent your school with pride.</p>
+            </div>
+            
+            <div className='content2'>
+                <h2>Shop</h2>
+                <ul>
+                    <li><a href="#">College</a></li>
+                    <li><a href="#">Senior High School</a></li>
+                    <li><a href="#">Merchandise</a></li>
+                </ul>
+            </div>
+            
+            <div className='content3'>
+                <h2>Socials</h2>
+                <a href="https://www.facebook.com/NUMOAph"><i className="fab fa-facebook"></i>: @NUMOAph</a>
+                <h2>Contact Us!</h2>
+                <a href="tel:+639985960424">(+63) 998 596 0424</a>
+            </div>
+        </div>
+    </section>
+
+
 
       <section className='feedbacksection'>
         <FeedbackPage />
