5312143d5c342d6949a8a3cdaab9195410724b05
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2024-10-25 16:31:40 +0800

    mikmik

--- systemfinals/src/components/CartPage.js
+++ systemfinals/src/components/CartPage.js
@@ -10,7 +10,7 @@ import NavBar from './NavBar';
 import ContactSection from './ContactSection';
 import ScrollToTopButton from './ScrollToTopButton';
 import FeedbackPage from './FeedbackPage';
-import ConfirmationModal from './ConfirmationModal'; 
+import ConfirmationModal from './ConfirmationModal';
 
 import product from "../imgs/merch/merch_nucap.png"; // ex. product image
 import product1 from "../imgs/college/college_femaleblouse.png"; // ex. product image
@@ -30,41 +30,41 @@ const CartPage = () => {
 
   const totalItems = () => quantities.reduce((sum, quantity) => sum + quantity, 0);
 
- // check if total quantity exceeds the limit
- const isLimitReached = () => totalItems() >= 5;
+  // check if total quantity exceeds the limit
+  const isLimitReached = () => totalItems() >= 5;
 
- const increaseQuantity = (index) => {
-   setQuantities(prevQuantities => {
-     const newQuantities = [...prevQuantities];
-     const product = products[index];
+  const increaseQuantity = (index) => {
+    setQuantities(prevQuantities => {
+      const newQuantities = [...prevQuantities];
+      const product = products[index];
 
-     // check if the total quantity is within the limit of 10
-     if (!isLimitReached()) {
-       // check if the product has a size and limit to 2, otherwise no limit
-       if (product.size) {
-         if (newQuantities[index] < 2) {
-           newQuantities[index] += 1;
-         }
-       } else {
-         newQuantities[index] += 1;
-       }
-     }
+      // check if the total quantity is within the limit of 10
+      if (!isLimitReached()) {
+        // check if the product has a size and limit to 2, otherwise no limit
+        if (product.size) {
+          if (newQuantities[index] < 2) {
+            newQuantities[index] += 1;
+          }
+        } else {
+          newQuantities[index] += 1;
+        }
+      }
 
-     return newQuantities;
-   });
- };
+      return newQuantities;
+    });
+  };
 
- const decreaseQuantity = (index) => {
-   setQuantities(prevQuantities => {
-     const newQuantities = [...prevQuantities];
-     if (newQuantities[index] > 1) {
-       newQuantities[index] -= 1;
-     }
-     return newQuantities;
-   });
- };
+  const decreaseQuantity = (index) => {
+    setQuantities(prevQuantities => {
+      const newQuantities = [...prevQuantities];
+      if (newQuantities[index] > 1) {
+        newQuantities[index] -= 1;
+      }
+      return newQuantities;
+    });
+  };
 
- // delete button function
+  // delete button function
   const removeProduct = (index) => {
     setQuantities(prevQuantities => prevQuantities.filter((_, i) => i !== index));
     setProducts(prevProducts => prevProducts.filter((_, i) => i !== index));
@@ -86,127 +86,127 @@ const CartPage = () => {
     const img = new Image();
     img.src = ordersummlogo;
 
-    img.onload = function() {
-        const logoWidth = img.width / 4;
-        const logoHeight = img.height / 4;
+    img.onload = function () {
+      const logoWidth = img.width / 4;
+      const logoHeight = img.height / 4;
 
-        // logo
-        const logoXPosition = margin;
-        doc.addImage(img, 'PNG', logoXPosition, margin, logoWidth, logoHeight);
+      // logo
+      const logoXPosition = margin;
+      doc.addImage(img, 'PNG', logoXPosition, margin, logoWidth, logoHeight);
 
-        const headerXPosition = doc.internal.pageSize.getWidth() - margin;
+      const headerXPosition = doc.internal.pageSize.getWidth() - margin;
 
-        // header
-        doc.setFont(undefined, 'bold');
-        doc.setTextColor(61, 59, 146);
-        doc.setFontSize(20);
-        const headerText = 'Bulldogs Exchange';
-        const headerTextWidth = doc.getTextWidth(headerText);
-        doc.text(headerText, headerXPosition - headerTextWidth, margin + (logoHeight / 2) + 5);
+      // header
+      doc.setFont(undefined, 'bold');
+      doc.setTextColor(61, 59, 146);
+      doc.setFontSize(20);
+      const headerText = 'Bulldogs Exchange';
+      const headerTextWidth = doc.getTextWidth(headerText);
+      doc.text(headerText, headerXPosition - headerTextWidth, margin + (logoHeight / 2) + 5);
 
-        doc.setFont(undefined, 'bold');
-        doc.setTextColor(0, 0, 0);
-        doc.setFontSize(14);
-        const orderSummaryText = 'Order Summary';
-        const orderSummaryTextWidth = doc.getTextWidth(orderSummaryText);
-        doc.text(orderSummaryText, headerXPosition - orderSummaryTextWidth, margin + (logoHeight / 2) + 15);
+      doc.setFont(undefined, 'bold');
+      doc.setTextColor(0, 0, 0);
+      doc.setFontSize(14);
+      const orderSummaryText = 'Order Summary';
+      const orderSummaryTextWidth = doc.getTextWidth(orderSummaryText);
+      doc.text(orderSummaryText, headerXPosition - orderSummaryTextWidth, margin + (logoHeight / 2) + 15);
 
-        const lineSpacingAfterTitle = 10;
-        doc.line(margin, margin + (logoHeight / 2) + 10 + lineSpacingAfterTitle, 210 - margin, margin + (logoHeight / 2) + 10 + lineSpacingAfterTitle);
+      const lineSpacingAfterTitle = 10;
+      doc.line(margin, margin + (logoHeight / 2) + 10 + lineSpacingAfterTitle, 210 - margin, margin + (logoHeight / 2) + 10 + lineSpacingAfterTitle);
 
-        const formattedDate = selectedDate ? format(selectedDate, 'MMMM d, yyyy') : 'Not selected'; // Format the date
+      const formattedDate = selectedDate ? format(selectedDate, 'MMMM d, yyyy') : 'Not selected'; // Format the date
 
-        // user info
-        let currentYPosition = margin + (logoHeight / 2) + 20 + lineSpacingAfterTitle;
-        doc.setFontSize(12);
-        doc.setFont(undefined, 'bold');
-        doc.text(`Name: Maria Nadine Faye Rufo`, margin, currentYPosition);
-        currentYPosition += lineSpacing;
-        doc.text(`Items: ${totalItems()}`, margin, currentYPosition);
-        currentYPosition += lineSpacing;
-        doc.text(`Pickup Date: ${formattedDate}`, margin, currentYPosition);
-        
-        currentYPosition += lineSpacing;
-        doc.line(margin, currentYPosition, 210 - margin, currentYPosition);
-        currentYPosition += lineSpacing;
+      // user info
+      let currentYPosition = margin + (logoHeight / 2) + 20 + lineSpacingAfterTitle;
+      doc.setFontSize(12);
+      doc.setFont(undefined, 'bold');
+      doc.text(`Name: Maria Nadine Faye Rufo`, margin, currentYPosition);
+      currentYPosition += lineSpacing;
+      doc.text(`Items: ${totalItems()}`, margin, currentYPosition);
+      currentYPosition += lineSpacing;
+      doc.text(`Pickup Date: ${formattedDate}`, margin, currentYPosition);
 
-        // products purchased
-        doc.setFontSize(12);
-        doc.setFont(undefined, 'bold');
-        doc.text('Products:', margin, currentYPosition);
-        
-        currentYPosition += lineSpacing;
+      currentYPosition += lineSpacing;
+      doc.line(margin, currentYPosition, 210 - margin, currentYPosition);
+      currentYPosition += lineSpacing;
 
-        doc.setFont(undefined, 'normal');
-        doc.setFontSize(10);
-        products.forEach((product, index) => {
-            const productYPosition = currentYPosition + index * (lineSpacing - 2);
-            doc.text(`${index + 1}. ${product.name}`, margin, productYPosition);
+      // products purchased
+      doc.setFontSize(12);
+      doc.setFont(undefined, 'bold');
+      doc.text('Products:', margin, currentYPosition);
 
-            const priceXPosition = 210 - margin;
-            doc.text(
-                `PHP ${product.price} x ${quantities[index]} = PHP ${quantities[index] * product.price}`,
-                priceXPosition,
-                productYPosition,
-                { align: 'right' }
-            );
-        });
+      currentYPosition += lineSpacing;
 
-        const totalYPosition = currentYPosition + products.length * (lineSpacing - 2);
-        doc.line(margin, totalYPosition + 5, 210 - margin, totalYPosition + 5);
+      doc.setFont(undefined, 'normal');
+      doc.setFontSize(10);
+      products.forEach((product, index) => {
+        const productYPosition = currentYPosition + index * (lineSpacing - 2);
+        doc.text(`${index + 1}. ${product.name}`, margin, productYPosition);
 
-        const totalPrice = quantities.reduce(
-            (sum, quantity, index) => sum + quantity * products[index].price,
-            0
+        const priceXPosition = 210 - margin;
+        doc.text(
+          `PHP ${product.price} x ${quantities[index]} = PHP ${quantities[index] * product.price}`,
+          priceXPosition,
+          productYPosition,
+          { align: 'right' }
         );
+      });
 
-        // total price
-        doc.setFontSize(12);
-        doc.setFont(undefined, 'bold');
-        doc.text(`Total Price: PHP ${totalPrice}`, 210 - margin, totalYPosition + 10, { align: 'right' });
+      const totalYPosition = currentYPosition + products.length * (lineSpacing - 2);
+      doc.line(margin, totalYPosition + 5, 210 - margin, totalYPosition + 5);
 
-        // barcode pic
-        const pageWidth = doc.internal.pageSize.getWidth();
-        const barcodeWidth = pageWidth - 2 * margin;
-        const barcodeHeight = (10 / 50) * barcodeWidth;
+      const totalPrice = quantities.reduce(
+        (sum, quantity, index) => sum + quantity * products[index].price,
+        0
+      );
 
-        currentYPosition = totalYPosition + 20;
-        doc.addImage(barcodeImage, 'PNG', margin, currentYPosition, barcodeWidth, barcodeHeight);
+      // total price
+      doc.setFontSize(12);
+      doc.setFont(undefined, 'bold');
+      doc.text(`Total Price: PHP ${totalPrice}`, 210 - margin, totalYPosition + 10, { align: 'right' });
 
-        currentYPosition += barcodeHeight + 5;
+      // barcode pic
+      const pageWidth = doc.internal.pageSize.getWidth();
+      const barcodeWidth = pageWidth - 2 * margin;
+      const barcodeHeight = (10 / 50) * barcodeWidth;
 
-        const orderIdXPosition = doc.internal.pageSize.getWidth() / 2;
-        doc.setFontSize(10);
+      currentYPosition = totalYPosition + 20;
+      doc.addImage(barcodeImage, 'PNG', margin, currentYPosition, barcodeWidth, barcodeHeight);
 
-        // get orderID
-        doc.text(`${orderId}`, orderIdXPosition, currentYPosition, { align: 'center' });
+      currentYPosition += barcodeHeight + 5;
 
-        currentYPosition += lineSpacing - 5;
+      const orderIdXPosition = doc.internal.pageSize.getWidth() / 2;
+      doc.setFontSize(10);
 
-        // footer
-        const thankYouMessage =
-            'Thank you for your purchase! Please present this along with your school ID to the cashier when you come to pick up your order.';
-        const messageXPosition = doc.internal.pageSize.getWidth() / 2;
-        const pageHeight = doc.internal.pageSize.getHeight();
-        const footerYPosition = pageHeight - 20;
+      // get orderID
+      doc.text(`${orderId}`, orderIdXPosition, currentYPosition, { align: 'center' });
 
-        doc.setFontSize(8);
-        doc.text(thankYouMessage, messageXPosition, footerYPosition, {
-            align: 'center',
-            baseline: 'middle',
-        });
+      currentYPosition += lineSpacing - 5;
 
-        // show orderId sa file name
-        const fileName = `Order_${orderId}_Rufo.pdf`; // user last name
-        doc.save(fileName);
+      // footer
+      const thankYouMessage =
+        'Thank you for your purchase! Please present this along with your school ID to the cashier when you come to pick up your order.';
+      const messageXPosition = doc.internal.pageSize.getWidth() / 2;
+      const pageHeight = doc.internal.pageSize.getHeight();
+      const footerYPosition = pageHeight - 20;
 
-        setQuantities([]);
-        setProducts([]);
+      doc.setFontSize(8);
+      doc.text(thankYouMessage, messageXPosition, footerYPosition, {
+        align: 'center',
+        baseline: 'middle',
+      });
+
+      // show orderId sa file name
+      const fileName = `Order_${orderId}_Rufo.pdf`; // user last name
+      doc.save(fileName);
+
+      setQuantities([]);
+      setProducts([]);
     };
-};
+  };
 
   const handleCheckout = () => {
-    generatePDF(); 
+    generatePDF();
     setIsModalOpen(false);
   };
 
@@ -215,7 +215,7 @@ const CartPage = () => {
     const today = new Date();
     const minDate = addDays(today, 0); // Day after today
     const maxDate = addDays(today, 11); // 10 days from today
-  
+
     // Check if date is within range and not a Sunday
     return date >= minDate && date <= maxDate && !isSunday(date);
   };
@@ -227,6 +227,14 @@ const CartPage = () => {
         <h1>Cart Summary</h1>
         <p>We take extra care to ensure your products are safely packaged and securely shipped, so they arrive fresh and in perfect condition every time!</p>
       </div>
+
+      <section>
+        <div className="notice-section">
+          <p>
+          Pickup Reminder: If you miss your pickup date, you can collect your order on another date, provided the items are in stock. Thank you!          
+          </p>
+        </div>
+      </section>
 
       <div className='cart-page'>
         {products.length === 0 ? (
@@ -286,16 +294,16 @@ const CartPage = () => {
           <div className='summary-item'>
             <p>Pickup Date:</p>
             {products.length > 0 ? (
-                <DatePicker
-                    selected={selectedDate}
-                    onChange={(date) => setSelectedDate(date)}
-                    filterDate={isDateValid}
-                    placeholderText="Select a Pickup Date"
-                    dateFormat="MMMM d, yyyy"
-                    className="react-datepicker__input"
-                />
+              <DatePicker
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
+                filterDate={isDateValid}
+                placeholderText="Select a Pickup Date"
+                dateFormat="MMMM d, yyyy"
+                className="react-datepicker__input"
+              />
             ) : (
-                <p></p>
+              <p></p>
             )}
           </div>
           <hr />
@@ -311,25 +319,25 @@ const CartPage = () => {
           </div>
 
           {products.length > 0 && (
-          <button 
-            onClick={() => {
-              if (!selectedDate) {
-                alert("Please select a pickup date before proceeding.");
-              } else {
-                setIsModalOpen(true);
-              }
-            }}
-            disabled={!selectedDate} // Disable the button if no date is selected
-          >
-            Proceed To Checkout
-          </button>
-        )}
+            <button
+              onClick={() => {
+                if (!selectedDate) {
+                  alert("Please select a pickup date before proceeding.");
+                } else {
+                  setIsModalOpen(true);
+                }
+              }}
+              disabled={!selectedDate} // Disable the button if no date is selected
+            >
+              Proceed To Checkout
+            </button>
+          )}
         </div>
 
         {/* for pop up window, checkout validation */}
-        <ConfirmationModal 
-          isOpen={isModalOpen} 
-          onClose={() => setIsModalOpen(false)} 
+        <ConfirmationModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
           onConfirm={handleCheckout}
         />
 
