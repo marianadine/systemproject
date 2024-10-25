9663c763037c933fda8bfd7cef15b287ae41f619
Maria Nadine Faye Rufo <nadinerufo7@gmail.com>
2024-10-25 14:58:53 +0800

    commit

--- systemfinals/src/components/ConfirmationModal.js
+++ systemfinals/src/components/ConfirmationModal.js
@@ -9,8 +9,10 @@ const ConfirmationModal = ({ isOpen, onClose, onConfirm }) => {
       <div className="modal-content">
         <h2>Confirmation</h2>
         <p>Are you sure you want to proceed?</p>
-        <button className='cancelbtn' onClick={onClose}>Cancel</button>
-        <button className='confirmbtn' onClick={onConfirm}>Confirm</button>
+        <div className="modal-actions">
+          <button className='cancelbtn' onClick={onClose}>Cancel</button>
+          <button className='confirmbtn' onClick={onConfirm}>Confirm</button>
+        </div>
       </div>
     </div>
   );
